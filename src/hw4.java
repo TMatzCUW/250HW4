d97302664e0567003f3cf5af98787282c9b55511
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2020-09-16 22:49:48 -0500

    Update hw4.java

--- src/hw4.java
+++ src/hw4.java
@@ -5,4 +5,16 @@ public class hw4
 	//binary to decimal
 	
 	//decimal to hexadecimal
+	public static String decimalToHex(int numberAsAnInt)
+	{
+		String answer = "";
+		String map = "0123456789ABCDEF";
+		while(numberAsAnInt != 0)
+		{
+			int temp = (numberAsAnInt % 16);
+			answer = map.charAt(temp) + answer;
+			numberAsAnInt = numberAsAnInt / 16;
+		}
+		return answer;
+	}
 }
